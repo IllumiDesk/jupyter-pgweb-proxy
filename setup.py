7c4467fc3bba33a2b0621bfada4178706ff2c324
Greg Werner <werner.greg@gmail.com>
2020-09-16 01:12:11 -0400

    chore: Release v0.2.0 (#8)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setuptools.setup(
     name="jupyter-pgweb-proxy",
-    version="0.1.1",
+    version="0.2.0",
     url="https://github.com/illumidesk/jupyter-pgweb-proxy",
     author="IllumiDesk Team",
     description="hello@illumidesk.com",
